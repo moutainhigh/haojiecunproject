35982673db9bd92edf4ad560add9145ea5f96924
zhangjiyuan66 <2290297571@qq.com>
2016-05-10 16:04:23 +0800

    1

---
+++ hjc_sp2p/app/models/v_return_rate_records.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import play.db.jpa.Model;
+
+/**
+ * 用户投返息记录
+* @author zb
+* @version 8.0
+* @created 2015年11月27日 下午4:14:14
+ */
+
+@Entity
+public class v_return_rate_records extends Model{
+	
+	public Date return_time;
+	public String invest_user_name;
+	public Double rate;
+	public String description;
+}
